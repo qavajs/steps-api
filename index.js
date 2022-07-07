432c7484902de1fc2853bcbb0782dc23314ba857
Kiryl Bahdanets <Kiryl_Bahdanets@epam.com>
2022-07-07 11:26:26 +0300

    Code review fixes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 /* eslint-disable import/no-unresolved */
 require('./lib/index.js');
+require('./lib/apiActionSteps.js');
+require('./lib/apiHelpers.js');
+require('./lib/apiVerificationSteps.js');
+require('./lib/hooks.js');
+require('./lib/parameterTypes.js');
+require('./lib/utils.js');
--- src/hooks.ts
+++ src/hooks.ts
@@ -4,7 +4,7 @@ import memory from '@qavajs/memory';
 /**
  * Before hook for saving user story file location in memory
  */
-Before(function (scenario) {
+Before(function (scenario: any) {
   const { uri }: any = scenario.gherkinDocument;
   memory.setValue('usFolder', uri);
 });
