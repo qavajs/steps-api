237644f525ad15a8190b4883ecc248a85f088b35
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-03-15 08:44:42 +0200

    added import test

---
+++ test/import.spec.ts
@@ -0,0 +1,15 @@
+import { jest, test, expect } from '@jest/globals';
+
+jest.mock('@cucumber/cucumber', () => ({
+    Given: jest.fn(),
+    When: jest.fn(),
+    Then: jest.fn(),
+    Before: jest.fn(),
+    After: jest.fn(),
+    defineParameterType: jest.fn()
+}));
+
+test('import steps', () => {
+    const importer = () => {import('../index.js')}
+    expect(importer).not.toThrowError();
+});
