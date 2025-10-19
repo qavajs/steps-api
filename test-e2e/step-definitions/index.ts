cabb0346eb9c139e74940e6e5e6d72f28e19c624
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-10-19 11:37:55 +0300

    dropped chai dependency

--- test-e2e/step-definitions/index.ts
+++ test-e2e/step-definitions/index.ts
@@ -1,15 +1,15 @@
 import { Then } from '@cucumber/cucumber';
-import { expect } from 'chai';
+import { expect } from '@qavajs/validation';
 import { MemoryValue } from '@qavajs/core';
 
 Then('I expect {value} memory value to be equal {value}', async function (actual: MemoryValue, expected: MemoryValue) {
   const actualValue = await actual.value();
   const expectedValue = await expected.value();
-  expect(actualValue).to.eql(expectedValue);
+  expect(actualValue).toEqual(expectedValue);
 });
 
 Then('I expect {value} memory value to contain {value}', async function (actual: MemoryValue, expected: MemoryValue) {
   const actualValue = await actual.value();
   const expectedValue = await expected.value();
-  expect(actualValue).contains(expectedValue);
+  expect(actualValue).toContain(expectedValue);
 });
